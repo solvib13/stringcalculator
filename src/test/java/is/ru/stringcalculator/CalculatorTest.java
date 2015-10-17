5730463d350df9c988b4ff92a32238da7ca1026d
solvib13 <solvib13@ru.is>
2015-10-17 21:17:08 +0000

    Newline delim. test and implementation

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -25,8 +25,13 @@ public class CalculatorTest {
 	}	
 
 	@Test
-    public void testMultipleNumbers(){
-    	assertEquals(6, Calculator.add("1,2,3"));
-    }
+    	public void testMultipleNumbers(){
+    		assertEquals(6, Calculator.add("1,2,3"));
+    	}
+	
+	@Test
+    	public void testNewlineDelim(){
+    		assertEquals(6, Calculator.add("1\n2,3"));
+    	}
 
 }
